b203ac97147ebcb04b2c1cca25c31501bc878529
Marcio Coelho Socorro <marcioc@gmail.com>
2022-04-19 14:37:52 -0300

    Integrating Discount.Grpc with Basket.API and Docker

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,13 +1,23 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddStackExchangeRedisCache(options => {
+builder.Services.AddStackExchangeRedisCache(options =>
+{
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+// Grpc
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+    (
+        o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"])
+    );
+builder.Services.AddScoped<DiscountGrpcService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
